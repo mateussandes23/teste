2f2c3c83a7f7dfaa6d6b75f15dd4f4955e9c86a6
Mateus Sandes <mateus.sandes@liferay.com>
2022-09-13 00:24:08 -0300

    my first code in java

---
+++ teste.java
@@ -0,0 +1,13 @@
+class prog1 {
+    public static void main (String[] args) {
+
+        int valor = 10;
+
+        if (valor > 0) {
+            System.out.printf("maior que 0");
+        }
+        else {
+            System.out.printf("menor que 0");
+        }
+    }
+}
